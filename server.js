da44fa79f55eb558c969520fe8ee4859395bc9dd
muunji <twoji3019@naver.com>
2024-12-03 15:15:22 +0900

    9020포트

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import http from 'node:http';
 
-const server = http.createServer((request, response)=>{
+const serverOne = http.createServer((request, response)=>{
   if (request.url === "/") {
     response.writeHead(200, {
       "Content-Type": "application/json",
@@ -23,6 +23,34 @@ const server = http.createServer((request, response)=>{
   }
 })
 
-server.listen(9010,()=>{
+serverOne.listen(9010,()=>{
   console.log("http://localhost:9010");
 })
+
+const serverTwo = http.createServer((request, response)=>{
+  if (request.url === "/") {
+    response.writeHead(200, {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    });
+
+    let data = {
+      password : "qwer1234!@"
+    }
+    let convertFormat = JSON.stringify(data);
+
+    response.write(convertFormat);
+    response.end();
+  } else {
+    response.writeHead(404, { "Content-Type": "application/json" });
+    response.write(JSON.stringify({ error: "Not Found" }));
+    response.end();
+  }
+})
+
+serverTwo.listen(9020,()=>{
+  console.log("http://localhost:9020");
+})
+
