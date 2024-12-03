845a6d36c806e9d6b8eccfeea8520aa9f10e35aa
muunji <twoji3019@naver.com>
2024-12-03 15:17:06 +0900

    9030포트

--- server.js
+++ server.js
@@ -54,3 +54,29 @@ serverTwo.listen(9020,()=>{
   console.log("http://localhost:9020");
 })
 
+const serverThree = http.createServer((request, response)=>{
+  if (request.url === "/") {
+    response.writeHead(200, {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type"
+    });
+
+    let data = {
+      phoneNumber : "010-1234-5678"
+    }
+    let convertFormat = JSON.stringify(data);
+
+    response.write(convertFormat);
+    response.end();
+  } else {
+    response.writeHead(404, { "Content-Type": "application/json" });
+    response.write(JSON.stringify({ error: "Not Found" }));
+    response.end();
+  }
+})
+
+serverThree.listen(9030,()=>{
+  console.log("http://localhost:9030");
+})
